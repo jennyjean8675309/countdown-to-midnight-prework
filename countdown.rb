4f4ea7ca268d4cacbc8a99294ce449afedbd757e
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-27 16:53:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,9 @@ def countdown(countdown_seconds)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(countdown_seconds)
+  while countdown_seconds > 0
+  puts "#{countdown_seconds} SECOND(S)!"
+  countdown_seconds -= 1
+end
